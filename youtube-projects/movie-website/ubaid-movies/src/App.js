926ccc67213374251394c364115fc050dc99f725
UbaidG <ughante@gmail.com>
2021-05-19 18:56:09 +0530

    final update

--- youtube-projects/movie-website/ubaid-movies/src/App.js
+++ youtube-projects/movie-website/ubaid-movies/src/App.js
@@ -4,9 +4,12 @@ import "./App.css";
 import MovieList from "./components/MovieList";
 import MovieListHeading from "./components/MovieListHeading";
 import SearchBox from "./components/SearchBox";
+import AddFav from "./components/AddFav";
+import RemoveFav from "./components/RemoveFav";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [fav, setFav] = useState([]);
   const [searchValue, setSearchValue] = useState("");
 
   const getMovieRequest = async (searchValue) => {
@@ -22,6 +25,26 @@ const App = () => {
     getMovieRequest(searchValue);
   }, [searchValue]);
 
+  useEffect(() => {
+    const movieFav = JSON.parse(localStorage.getItem("react-movie-app-fav"));
+    setFav(movieFav);
+  }, []);
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem("react-movie-app-fav", JSON.stringify(items));
+  };
+  const addFavMovie = (movie) => {
+    const newFavList = [...fav, movie];
+    setFav(newFavList);
+    saveToLocalStorage(newFavList);
+  };
+
+  const removeFavMovie = (movie) => {
+    const newFavList = fav.filter((fav) => fav.imdbID !== movie.imdbID);
+    setFav(newFavList);
+    saveToLocalStorage(newFavList);
+  };
+
   return (
     <div className="container-fluid movie-app">
       <div className="row d-flex align-items-center mt-4 mb-4">
@@ -29,7 +52,21 @@ const App = () => {
         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
       <div className="row">
-        <MovieList movies={movies} />
+        <MovieList
+          movies={movies}
+          handleFavClick={addFavMovie}
+          favComponent={AddFav}
+        />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favourites" />
+      </div>
+      <div className="row">
+        <MovieList
+          movies={fav}
+          handleFavClick={removeFavMovie}
+          favComponent={RemoveFav}
+        />
       </div>
     </div>
   );
